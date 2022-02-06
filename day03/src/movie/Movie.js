5356b1b027da2c9d631308d6e8d06e640c218dc3
standup8 <borapark13@gmail.com>
2022-02-06 23:37:28 +0900

    Add: day07 directory add

--- day03/src/movie/Movie.js
+++ day03/src/movie/Movie.js
@@ -1,18 +1,18 @@
-import React, {useState} from 'react'
-import movieData from '../assets/api/movieData';
-import MovieList from './MovieList';
-import './style.css';
+import React, { useState } from "react";
+import movieData from "../assets/api/movieData";
+import MovieList from "./MovieList";
+import "./style.css";
 
 export default function Movie() {
-    const [movies, setMovies] = useState(movieData)
+  const [movies, setMovies] = useState(movieData);
 
-    const removeItem = (number)=> {
-        setMovies(movies.filter(movie => movie.no !== number))
-    }
+  const removeItem = (id) => {
+    setMovies(movies.filter((movie) => movie.no !== id));
+  };
 
-    return (
-        <div className='wrap'>
-            <MovieList movies={movies} removeItem={removeItem}/>
-        </div>
-    )
+  return (
+    <div className="wrap">
+      <MovieList movies={movies} removeItem={removeItem} />
+    </div>
+  );
 }
